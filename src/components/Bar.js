8c08a852c7f150a6fabbb18f90128f7e02544d17
bytekn1ght <64689096+bytekn1ght@users.noreply.github.com>
2021-08-30 17:07:56 +0530

    Add files via upload

---
+++ src/components/Bar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Bar.css"
+
+function Bar({
+    length,
+    colorKey
+}) {
+    const COLOR_SET = ["grey", "red", "green"];
+
+    let color = COLOR_SET[colorKey];
+
+    let style = {
+        height: length,
+        backgroundColor: color,
+    }
+
+    return ( < div className = "bar"
+        style = {
+            style
+        } > < /div>
+    )
+}
+
+export default Bar;
